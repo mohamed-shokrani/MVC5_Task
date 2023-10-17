552bb1b9f2bd66db0508ff3c691b1deb45ff4e86
mohamed-shokrani <mohamedshokrani55@gmail.com>
2023-10-17 09:06:47 +0300

    handle the error if exists

--- MVC5_Task/Services/EmployeeService.cs
+++ MVC5_Task/Services/EmployeeService.cs
@@ -48,48 +48,56 @@ namespace MVC5_Task.Services
         }
         private byte[] Report(IReadOnlyList<EmployeeProjectDto> employees)
         {
-            using MemoryStream stream = new();
-
-            PdfSharpCore.Pdf.PdfDocument pdf = new PdfSharpCore.Pdf.PdfDocument();
-            PdfSharpCore.Pdf.PdfPage page = pdf.AddPage();
-
-            XGraphics graphics = XGraphics.FromPdfPage(page);
-            XFont font = new XFont("Arial", 12, XFontStyle.Bold);
-
-            int y = 20;
-            string title = "Employee Report";
-            XSize titleSize = graphics.MeasureString(title, font);
-            double titleX = (page.Width - titleSize.Width) / 2;
-            double titleY = y;
-            graphics.DrawString(title, font, XBrushes.Black, new XPoint(titleX, titleY));
-            y = 40;
-            graphics.DrawString("Employee Name", font, XBrushes.Black, new XPoint(50, y));
-            graphics.DrawString("Department", font, XBrushes.Black, new XPoint(200, y));
-            graphics.DrawString("Project Name", font, XBrushes.Black, new XPoint(500, y));
-            graphics.DrawString("Total Hours", font, XBrushes.Black, new XPoint(350, y));
-            y += 40; ;
-            foreach (var employee in employees)
+            try
             {
-                graphics.DrawString(employee.EmployeeName, font, XBrushes.Gray, new XPoint(50, y));
-                graphics.DrawString(employee.DepartmentName, font, XBrushes.Gray, new XPoint(200, y));
-                graphics.DrawString(employee.TotalHours.ToString(), font, XBrushes.Gray, new XPoint(350, y));
-                graphics.DrawString(employee.ProjectName, font, XBrushes.Gray, new XPoint(500, y));
+                using MemoryStream stream = new();
 
-                y += employees.Count;
+                PdfSharpCore.Pdf.PdfDocument pdf = new PdfSharpCore.Pdf.PdfDocument();
+                PdfSharpCore.Pdf.PdfPage page = pdf.AddPage();
+
+                XGraphics graphics = XGraphics.FromPdfPage(page);
+                XFont font = new XFont("Arial", 12, XFontStyle.Bold);
+
+                int y = 20;
+                string title = "Employee Report";
+                XSize titleSize = graphics.MeasureString(title, font);
+                double titleX = (page.Width - titleSize.Width) / 2;
+                double titleY = y;
+                graphics.DrawString(title, font, XBrushes.Black, new XPoint(titleX, titleY));
+                y = 40;
+                graphics.DrawString("Employee Name", font, XBrushes.Black, new XPoint(50, y));
+                graphics.DrawString("Department", font, XBrushes.Black, new XPoint(200, y));
+                graphics.DrawString("Project Name", font, XBrushes.Black, new XPoint(500, y));
+                graphics.DrawString("Total Hours", font, XBrushes.Black, new XPoint(350, y));
+                y += 40; ;
+                foreach (var employee in employees)
+                {
+                    graphics.DrawString(employee.EmployeeName, font, XBrushes.Gray, new XPoint(50, y));
+                    graphics.DrawString(employee.DepartmentName, font, XBrushes.Gray, new XPoint(200, y));
+                    graphics.DrawString(employee.TotalHours.ToString(), font, XBrushes.Gray, new XPoint(350, y));
+                    graphics.DrawString(employee.ProjectName, font, XBrushes.Gray, new XPoint(500, y));
+
+                    y += employees.Count;
+                }
+                // Add page number
+                var pageNumberFont = new XFont("Arial", 10, XFontStyle.Regular);
+                var pageNumberText = string.Format("Page {0} of {1}", 1, 1);
+                graphics.DrawString(pageNumberText, pageNumberFont, XBrushes.Black, new XPoint(50, page.Height - 50));
+
+                // Add current date and time
+                var dateTimeNow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                var dateTimeFont = new XFont("Arial", 10, XFontStyle.Regular);
+                graphics.DrawString(dateTimeNow, dateTimeFont, XBrushes.Black, new XPoint(page.Width - 200, page.Height - 50));
+                pdf.Save(stream);
+                stream.Position = 0;
+
+                return stream.ToArray();
             }
-            // Add page number
-            var pageNumberFont = new XFont("Arial", 10, XFontStyle.Regular);
-            var pageNumberText = string.Format("Page {0} of {1}", 1, 1);
-            graphics.DrawString(pageNumberText, pageNumberFont, XBrushes.Black, new XPoint(50, page.Height - 50));
-
-            // Add current date and time
-            var dateTimeNow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-            var dateTimeFont = new XFont("Arial", 10, XFontStyle.Regular);
-            graphics.DrawString(dateTimeNow, dateTimeFont, XBrushes.Black, new XPoint(page.Width - 200, page.Height - 50));
-            pdf.Save(stream);
-            stream.Position = 0;
-
-            return stream.ToArray();
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while generating the report: {ex.Message}");
+                return null; 
+            }
         }
     }
 }
